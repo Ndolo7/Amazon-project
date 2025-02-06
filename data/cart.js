a1332b648a90b3f7cbebb8f4df62fd9bf3bd5b74
Ndolo <imanindolo77@gmail.com>
2025-02-06 08:03:42 +0300

    my first commit...javascript coded

---
+++ data/cart.js
@@ -0,0 +1,66 @@
+
+//Get stingified JSON data as an array
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+
+if (!cart) {     //set default value for empty cart
+    cart = [{
+        productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: '1'
+    }, {
+        productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: '2'
+    }];
+}
+
+
+//save he data to local storage as a string
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+
+
+export function addToCart(productId) {
+    let matchingItem;
+
+    cart.forEach((item) => {
+        if (productId === item.productId) {
+            matchingItem = item;
+        }
+    });
+
+    if (matchingItem) {
+        matchingItem.quantity += 1;
+        
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: 1,
+            deliveryOptionId: '1'
+        }); 
+    }
+
+    saveToStorage();
+}
+
+export function removeFromCart(productId) {
+    /* create new array
+    Loop thru the cart
+    Add each cart product into the new array, except for this deleted productID */
+    
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+    
+    cart = newCart;
+
+    saveToStorage();
+
+}
