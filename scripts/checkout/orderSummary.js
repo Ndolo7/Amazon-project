4a4954411adff2b4ffe6c0a02ac8434fa513882d
Ndolo <imanindolo77@gmail.com>
2025-02-21 18:02:29 +0300

    made the payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,8 +1,9 @@
 import {cart, addToCart, removeFromCart, updateDeliveryOption} from "../../data/cart.js"; 
-import {products} from "../../data/products.js";
+import {products, getProduct} from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { deliveryOptions } from "../../data/deliveryOptions.js"; 
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js"; 
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 const today = dayjs();
 const deliveryDate = today.add(7, 'days');
@@ -17,24 +18,12 @@ export function renderOrderSummary() {
 
         const productId = cartItem.productId;
 
-        let matchingProduct;
-
-        products.forEach((product) => {
-            if(product.id === productId) {
-                matchingProduct = product;
-            }
-            
-        });
+        const matchingProduct = getProduct(productId);
 
 
         const deliveryOptionId = cartItem.deliveryOptionId;
-        let deliveryOption;
-
-        deliveryOptions.forEach((option) => {
-            if (option.id === deliveryOptionId) {
-                deliveryOption = option;
-            }
-        });
+        
+        const deliveryOption = getDeliveryOption(deliveryOptionId);
 
         const today = dayjs();
             const deliveryDate = today.add(
@@ -153,6 +142,8 @@ export function renderOrderSummary() {
                     `.js-cart-item-container-${productId}`
                 );
                 container.remove();
+                renderOrderSummary();
+                renderPaymentSummary();
             });
         });
             
@@ -163,6 +154,7 @@ export function renderOrderSummary() {
                 updateDeliveryOption(productId, deliveryOptionId);
 
                 renderOrderSummary();
+                renderPaymentSummary();
             });
         });
     
